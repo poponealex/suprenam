d8056fe220617a2ff462766e4ada663067c597b4
laowantong <aristide.grange@univ-lorraine.fr>
2021-04-06 13:25:34 +0200

    add various convenience color printing functions

---
+++ src/goodies.py
@@ -0,0 +1,28 @@
+import sys
+
+# ANSI color codes
+OK = "\033[92m"
+WARNING = "\033[1m\033[38;5;166m"
+FAIL = "\033[1m\033[91m"
+RESET = "\033[0m"
+
+
+def print_success(message: str):
+    """Print the given message of success."""
+    print(f"{OK}{message}{RESET}")
+
+
+def print_warning(message: str):
+    """Print the given message on `sys.stderr`."""
+    print(f"{WARNING}Warning: {message}{RESET}", file=sys.stderr)
+
+
+def print_fail(message: str):
+    """Print the given message on `sys.stderr` and raise a ValueError."""
+    print(f"{FAIL}Error: {message}{RESET}", file=sys.stderr)
+    raise ValueError(message)
+
+
+def print_exit(message: str):
+    """Print the given message on `sys.stderr` and exit."""
+    sys.exit(f"{FAIL}Error: {message}{RESET}")
