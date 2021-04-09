c8e58419a3ce29e270c71c3c7f619368361cbe78
poponealex <74072635+poponealex@users.noreply.github.com>
2021-04-09 13:15:45 +0200

    Fixing complexity issue

--- src/path_siblings.py
+++ src/path_siblings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 def get_path_siblings(paths: set[Path]) -> set[Path]:
     acc = set()
     for path in paths:
-        for p in Path(path.parent).glob("*"):
-            acc.add(p)
+        if path not in acc:
+            for p in Path(path.parent).glob("*"):
+                acc.add(p)
     return paths.union(acc)
