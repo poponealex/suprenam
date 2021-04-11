f96e307f4d7cbf6e953e9c3cb579ca18888e9577
poponealex <74072635+poponealex@users.noreply.github.com>
2021-04-11 21:45:24 +0200

    Add parameter to create_fhs

--- test/create_fhs.py
+++ test/create_fhs.py
@@ -1,10 +1,12 @@
+__import__("sys").path[0:0] = ["."]
 from pathlib import Path
 
-ROOT = "FHS"
-PATHS = Path("fhs.txt").read_text().split("\n")[:-1]
 
-def create_fhs():
-    if not Path(ROOT).exists():
-        Path(ROOT).mkdir()
+PATHS = Path("test/fhs.txt").read_text().split("\n")[:-1]
+
+
+def create_fhs(root="test/FHS"):
+    if not Path(root).exists():
+        Path(root).mkdir()
         for path in PATHS:
-            Path(f"{ROOT}{path}").mkdir()
+            Path(f"{root}{path}").mkdir()
