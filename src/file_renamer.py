1659f30a2e0971776405acae7414a8e8a5d4b620
poponealex <74072635+poponealex@users.noreply.github.com>
2021-04-11 18:08:07 +0200

    Fixing importation issues

--- src/file_renamer.py
+++ src/file_renamer.py
@@ -1,10 +1,11 @@
+__import__("sys").path[0:0] = ["."]
 import re, sys, os
 from pathlib import Path, PurePath
 from typing import NamedTuple, Union, Generator, Callable
 from itertools import count
 from tempfile import NamedTemporaryFile
 from tkinter import messagebox
-from file_system import FileSystem
+from src.file_system import FileSystem
 
 
 class Color:
