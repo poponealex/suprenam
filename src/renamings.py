eeb8811060cf7c4618586890ba0fdf0991858930
laowantong <laowantong@users.noreply.github.com>
2021-09-09 16:01:57 +0200

    refactor and add the capability to undo logged renamings

--- src/renamings.py
+++ src/renamings.py
@@ -1,30 +1,52 @@
-from typing import List
-from src.user_types import Clause
-
-from src.goodies import print_success, print_exit
-
+from pathlib import Path
 import logging
+import re
+from typing import List
+
+from src.goodies import print_exit, print_success
+from src.user_types import Renaming
 
 
-def perform_renamings(clauses: List[Clause], log_file_path):
-    sources_and_targets = [(path, path.parent / new_name) for (path, new_name) in clauses]
-    logging.basicConfig(filename=log_file_path, filemode="w", level=logging.DEBUG)
+def set_logger(log_path: Path):
+    logging.basicConfig(filename=log_path, filemode="w", level=logging.DEBUG)
+
+
+def perform_renamings(renamings: List[Renaming]):
     try:
-        for (i, (source, target)) in enumerate(sources_and_targets):
-            source.rename(target)
-            logging.info(f"{source} > {target}")
+        for (i, renaming) in enumerate(renamings):
+            rename_and_log(renaming)
         print_success("Success.")
     except OSError:
-        rollback_renamings(reversed(sources_and_targets[:i]), log_file_path)
+        logging.warning("Failed renaming.")
+        rollback_renamings(renamings[:i])
 
 
-def rollback_renamings(targets_and_sources, log_file_path):
+def rollback_renamings(renamings: List[Renaming]):
     try:
-        for (target, source) in targets_and_sources:
-            source.rename(target)
-            logging.info(f"{source} > {target}")
+        for renaming in reversed(renamings):
+            rename_and_log(swap(renaming))
         logging.warning("Rolled back.")
         print_exit("Recoverable error during the renaming. No changes.")
     except OSError:
-        logging.critical("Failed.")
-        print_exit(f"Unrecoverable error during the renaming, see '{log_file_path}'.")
+        logging.critical("Failed rollback.")
+        log_path = logging.getLoggerClass().root.handlers[0].baseFilename
+        print_exit(f"Unrecoverable error during the renaming, see '{log_path}'.")
+
+
+def rename_and_log(renaming: Renaming):
+    renaming.source.rename(renaming.target)
+    logging.info(f"SOURCE:{renaming.source}")
+    logging.info(f"TARGET:{renaming.target}")
+
+
+def swap(renaming: Renaming) -> Renaming:
+    """Return a copy of the given renaming where the source and the target are swapped."""
+    return Renaming(Path(renaming.target), Path(renaming.source))
+
+
+def undo_renamings(
+    log_path: Path,
+    find_all_renamings=re.compile(r"(?m)^INFO:\w+:SOURCE:(.+)\nINFO:\w+:TARGET:(.+)").findall,
+):
+    """Read a log file and apply the renamings in reversed and by swapping sources and targets."""
+    perform_renamings(list(map(swap, reversed(find_all_renamings(log_path.read_text())))))
--- test/test_renamings.py
+++ test/test_renamings.py
@@ -1,10 +1,11 @@
-import pytest
 
 from pathlib import Path
 
 import context
-from src.renamings import perform_renamings
+from src.renamings import *
+from src.user_types import Renaming
 
+import pytest
 
 def rm_tree(path: Path):  # https://stackoverflow.com/a/57892171/173003
     if not path.is_dir():
@@ -22,48 +23,44 @@ def test_happy_path():
     base = Path("test") / "happy_path"
     rm_tree(base)
     base.mkdir()
-    source_paths = [
-        base / "source_1",
-        base / "source_2",
-        base / "source_3",
+    renamings = [
+        Renaming(base / "source_1", base / "target_1"),
+        Renaming(base / "source_2", base / "target_2"),
+        Renaming(base / "source_3", base / "target_3"),
     ]
-    for source_path in source_paths:
-        source_path.touch()
-    target_names = [
-        "target_1",
-        "target_2",
-        "target_3",
-    ]
-    perform_renamings(zip(source_paths, target_names), "log.txt")
+    for renaming in renamings:
+        renaming.source.touch()
+    set_logger(Path("test/happy_log.txt"))
+    perform_renamings(renamings)
     try:
-        assert {path.name for path in base.iterdir()} == set(target_names)
+        assert set(base.iterdir()) == set(renaming.target for renaming in renamings)
+        # TODO: uncomment the following lines and try to understand why no logging file
+        #       is created when launched by pytest.
+        # undo_renamings(Path("test/happy_log.txt"))
+        # assert set(base.iterdir()) == set(renaming.source for renaming in renamings)
     finally:
         rm_tree(base)
+    
 
 def test_rollback_path():
     """Test the case where a renaming fails, and the previous ones are reverted."""
     base = Path("test") / "rollback_path"
     rm_tree(base)
     base.mkdir()
-    source_paths = [
-        base / "source_1",
-        base / "source_2",
-        base / "source_3", # to be deleted
-        base / "source_4",
+    renamings = [
+        Renaming(base / "source_1", base / "target_1"),
+        Renaming(base / "source_2", base / "target_2"),
+        Renaming(base / "source_3", base / "target_3"),
+        Renaming(base / "source_4", base / "target_4"),
     ]
-    for source_path in source_paths:
-        source_path.touch()
-    (base / "source_3").unlink()
-    target_names = [
-        "target_1",
-        "target_2",
-        "target_3",
-        "target_4",
-    ]
+    for renaming in renamings:
+        renaming.source.touch()
+    renamings[2].source.unlink() # delete a source
+    set_logger(Path("test/rollback_log.txt"))
     with pytest.raises(SystemExit):
-        perform_renamings(zip(source_paths, target_names), "log.txt")
+        perform_renamings(renamings)
     try:
-        assert {path.name for path in base.iterdir()} == {"source_1", "source_2", "source_4"}
+        assert set(base.iterdir()) == {base / "source_1", base / "source_2", base / "source_4"}
     finally:
         rm_tree(base)
 
