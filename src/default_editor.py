f8701275045ae36817da24dc3774047dc345bfe7
poponealex <74072635+poponealex@users.noreply.github.com>
2021-09-12 22:01:31 +0200

    Update re.compile call and macOS REGEX
    Remove unnecessary re.DOTALL parameter in the re.compile call and update OS["macOS"]["REGEX"]

--- src/default_editor.py
+++ src/default_editor.py
@@ -6,7 +6,7 @@ from platform import platform
 OS = {
     "macOS": {
         "QUERY_DEFAULTS_COMMAND": ["defaults", "read", "com.apple.LaunchServices/com.apple.launchservices.secure", "LSHandlers"],
-        "REGEX": r'(?m)\s*\{\s*LSHandlerContentType = "public\.plain-text";\s*LSHandlerPreferredVersions =\s*\{\s*LSHandlerRoleAll = "-";\s*\};\s*LSHandlerRoleAll = "([\w.]+)";',
+        "REGEX": r'(?m)(?s)\s*\{\s*LSHandlerContentType = "public\.plain-text";\s*LSHandlerPreferredVersions =\s*\{\s*LSHandlerRoleAll = "-";\s*\};\s*LSHandlerRoleAll = "([\w.]+)";',
         "DEFAULT_COMMAND": ["open", "-neW"],
         "EDITOR_COMMAND": {"com.microsoft.vscode": ["code", "-w"], "com.sublimetext.3": ["subl", "-w"]},
     },
@@ -36,7 +36,7 @@ def get_editor_command_name(os_name: str = "") -> str:
     os = OS.get(os_name, None)
     if not os:
         raise UnsupportedOS("OS not yet supported.")
-    parse_output = re.compile(os["REGEX"], re.DOTALL).findall
+    parse_output = re.compile(os["REGEX"]).findall
     try:
         result = parse_output(subprocess.run(os["QUERY_DEFAULTS_COMMAND"], stdout=subprocess.PIPE).stdout.decode("utf-8"))
         if result:
