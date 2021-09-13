3a4d910e000dabca02034dff78d7936f2d379271
poponealex <74072635+poponealex@users.noreply.github.com>
2021-09-13 22:58:51 +0200

    Add tests for `suprenam.py`
    Add test for `get_text()` and `parse_temporary_file()`.
    Remove test for the temporary file.

--- test/test_suprenam.py
+++ test/test_suprenam.py
@@ -5,10 +5,24 @@ from pathlib import Path
 import context
 from src.suprenam import *
 from test_renamings import rm_tree
-from src.user_types import Clause
 
 
-def test_temporary_file():
+def test_get_text():
+    base = Path("test") / "temp"
+    rm_tree(base)
+    base.mkdir()
+    parent = Path.cwd() / "test" / "temp"
+    paths = [base / "foo", base / "bar", base / "spam"]
+    for path in paths:
+        path.touch()
+    actual = get_text(paths)
+    expected = f"{parent}\n\n{paths[0].stat().st_ino}\tfoo\n{paths[1].stat().st_ino}\tbar\n{paths[2].stat().st_ino}\tspam\n"
+    print(expected)
+    assert actual == expected
+    rm_tree(base)
+
+
+def test_parse_temporary_file():
     base = Path("test") / "temp"
     rm_tree(base)
     base.mkdir()
@@ -17,13 +31,11 @@ def test_temporary_file():
     for path in paths:
         path.touch()
     population = {path.stat().st_ino: path for path in paths}
-    temp_file = create_temporary_file(paths)
-    expected = f"{parent}\n\n{paths[0].stat().st_ino}\tfoo\n{paths[1].stat().st_ino}\tbar\n{paths[2].stat().st_ino}\tspam\n"
-    print(temp_file.read_text())
-    assert expected == temp_file.read_text()
-    temp_file.write_text(f"{parent}\n\n{paths[0].stat().st_ino}\tboo\n{paths[1].stat().st_ino}\ttar\n{paths[2].stat().st_ino}\tbam\n")
-    expected = [Clause(paths[0], "boo"), Clause(paths[1], "tar"), Clause(paths[2], "bam")]
+    temp_file = Path(base) / "temp.txt"
+    temp_file.touch()
+    temp_file.write_text(f"{parent}\n\n{paths[0].stat().st_ino}\tfoo\n{paths[1].stat().st_ino}\tbar\n{paths[2].stat().st_ino}\tspam\n")
     actual = parse_temporary_file(temp_file, population)
+    expected = [Clause(paths[0], "foo"), Clause(paths[1], "bar"), Clause(paths[2], "spam")]
     assert actual == expected
     rm_tree(base)
 
