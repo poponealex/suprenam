61555c1baee36be6573950015ad9bc9dcbe1fc61
laowantong <laowantong@users.noreply.github.com>
2021-09-15 13:41:11 +0200

    add more tests

--- test/test_edition.py
+++ test/test_edition.py
@@ -5,42 +5,74 @@ from src.edition import *
 
 import pytest
 
-editable_text_dataset = [
+editable_text_dataset = [  # NB: in the triple-quoted strings, inodes and paths are tab-separated.
+    # Rename nothing -> empty result (TODO check if this case can occur or is filtered out before)
+    (
+        {},
+        "",
+    ),
+    # Rename empty path -> empty string (TODO check if this case can occur or is filtered out before)
     (
         {
-            1421832123: Path("/usr/bin/X11"),
+            2612647906: Path(),
         },
         """
-            /usr/bin
-            1421832123	X11
-        """,  # NB: inodes and paths are tab-separated
+            2612647906	
+        """,
     ),
+    # Rename root -> empty string (TODO check if this case can occur or is filtered out before)
+    (
+        {
+            2612647906: Path("/"),
+        },
+        """
+            2612647906	
+        """,
+    ),
+    # Rename a bunch of siblings -> their parent is not displayed
+    (
+        {
+            2612647906: Path("/usr/lib/gcc-lib"),
+            3263123972: Path("/usr/lib/locale"),
+            1549534358: Path("/usr/lib/X11"),
+            1582956841: Path("/usr/lib/tls"),
+            1981263235: Path("/usr/lib/games"),
+        },
+        """
+            1549534358	X11
+            1981263235	games
+            2612647906	gcc-lib
+            3263123972	locale
+            1582956841	tls
+        """,
+    ),
+    # Rename an arbitrary arborescence -> the siblings are grouped by parents, and sorted
     (
         {
             1421832123: Path("/usr/bin/X11"),
             2612647906: Path("/usr/lib/gcc-lib"),
             2784148792: Path("/usr/lib"),
             3263123972: Path("/usr/lib/locale"),
-            482466386: Path("/usr/bin"),
-            549534358: Path("/usr/lib/X11"),
-            582956841: Path("/usr/lib/tls"),
-            981263235: Path("/usr/lib/games"),
+            1482466386: Path("/usr/bin"),
+            1549534358: Path("/usr/lib/X11"),
+            1582956841: Path("/usr/lib/tls"),
+            1981263235: Path("/usr/lib/games"),
         },
         """
             /usr
-            482466386	bin
+            1482466386	bin
             2784148792	lib
     
             /usr/bin
             1421832123	X11
     
             /usr/lib
-            549534358	X11
-            981263235	games
+            1549534358	X11
+            1981263235	games
             2612647906	gcc-lib
             3263123972	locale
-            582956841	tls
-        """,  # NB: inodes and paths are tab-separated
+            1582956841	tls
+        """,
     ),
 ]
 
