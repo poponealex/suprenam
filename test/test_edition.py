5800c65a41e748ccebb3066584d5b9edc05ae0ed
poponealex <74072635+poponealex@users.noreply.github.com>
2021-09-20 20:06:37 +0200

    Improve tests

--- test/test_edition.py
+++ test/test_edition.py
@@ -89,92 +89,230 @@ def test_get_editable_text(title, inode_paths, expected):
 
 edited_text_dataset = [
     (
-        {},
+        "Parse a file which doesn't end with a newline.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            123	foobar
+            456	club""",
+        [
+            Clause(Path("/home/foo"), "foobar"),
+            Clause(Path("/home/bar"), "club"),
+        ],
+    ),
+    (
+        "Parse a file which is missing an inode.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            	foobar
+            456	club
+        """,
+        [
+            Clause(Path("/home/bar"), "club"),
+        ],
+    ),
+    (
+        "Parse a file which is missing multiple inodes.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            	foobar
+            	club
+        """,
+        [],
+    ),
+    (
+        "Parse a file which is missing a name.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            123	
+            456	club
+        """,
+        [
+            Clause(Path("/home/bar"), "club"),
+        ],
+    ),
+    (
+        "Parse a file which is missing multiple names.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            123	
+            456	
+        """,
+        [],
+    ),
+    (
+        "Parse a file which contains a name made of whitespaces.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            123	    
+            456	club
+        """,
+        [
+            Clause(Path("/home/bar"), "club"),
+        ],
+    ),
+    (
+        "Parse a file which contains a line with multiple tabs.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            123				foobar
+            456	club
+        """,
+        [
+            Clause(Path("/home/bar"), "club"),
+        ],
+    ),
+    (
+        "Parse a file which contains a name that is only digits.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            123	12345
+            456	club
+        """,
+        [
+            Clause(Path("/home/foo"), "12345"),
+            Clause(Path("/home/bar"), "club"),
+        ],
+    ),
+    (
+        "Parse a file which contains too many inodes (superset).",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            123	foobar
+            456	club
+            789 spam
+        """,
+        [
+            Clause(Path("/home/foo"), "foobar"),
+            Clause(Path("/home/bar"), "club"),
+        ],
+    ),
+    (
+        "Parse a file which doesn't contain all the inodes (subset).",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
+        """
+            123	foobar
+        """,
+        [
+            Clause(Path("/home/foo"), "foobar"),
+        ],
+    ),
+    (
+        "Parse a file which only contains different inodes from the original ones (symmetric difference).",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+            789: Path("/home/spam"),
+        },
+        """
+            1234	foobar
+            4567	club
+            7890	spam
+        """,
+        [],
+    ),
+    (
+        "Parse a file which doesn't contain any modification.",
+        {123: Path("/home/foo"), 456: Path("/home/bar")},
+        """
+            123	foo
+            456	bar
+        """,
+        [],
+    ),
+    (
+        "Parse a file which is empty.",
+        {
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+        },
         "",
         [],
     ),
-    ({123456: Path()}, "123456", []),
     (
-        {123456: Path("/foo")},
-        """
-            123456	bar
-        """,
-        [Clause(Path("/foo"), "bar")],
-    ),
-    (
-        # Rename an arbitrary arborescence -> the siblings are grouped by parents, and sorted
+        "Parse a file which doesn't contain any errors.",
         {
-            2612647906: Path("/usr/lib/gcc-lib"),
-            3263123972: Path("/usr/lib/locale"),
-            1549534358: Path("/usr/lib/X11"),
-            1582956841: Path("/usr/lib/tls"),
-            1981263235: Path("/usr/lib/games"),
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
         },
         """
-            1549534358	X12
-            1981263235	play
-            2612647906	lib-gcc
-            3263123972	foreign
-            1582956841	osi
+            123	foobar
+            456	club
         """,
         [
-            Clause(Path("/usr/lib/X11"), "X12"),
-            Clause(Path("/usr/lib/games"), "play"),
-            Clause(Path("/usr/lib/gcc-lib"), "lib-gcc"),
-            Clause(Path("/usr/lib/locale"), "foreign"),
-            Clause(Path("/usr/lib/tls"), "osi"),
+            Clause(Path("/home/foo"), "foobar"),
+            Clause(Path("/home/bar"), "club"),
         ],
     ),
-    # Rename an arbitrary arborescence -> the siblings are grouped by parents, and sorted
     (
+        "Parse a file which contains paths with different parents.",
         {
-            1421832123: Path("/usr/bin/X11"),
-            2612647906: Path("/usr/lib/gcc-lib"),
-            2784148792: Path("/usr/lib"),
-            3263123972: Path("/usr/lib/locale"),
-            1482466386: Path("/usr/bin"),
-            1549534358: Path("/usr/lib/X11"),
-            1582956841: Path("/usr/lib/tls"),
-            1981263235: Path("/usr/lib/games"),
+            123: Path("/home/foo"),
+            456: Path("/home/bar"),
+            789: Path("/etc/lib"),
+            999: Path("/hello/world"),
         },
         """
-            /usr
-            1482466386	garbage
-            2784148792	library
-    
-            /usr/bin
-            1421832123	X10
-    
-            /usr/lib
-            1549534358	X12
-            1981263235	play
-            2612647906	lib-gcc
-            3263123972	foreign
-            1582956841	osi
+            /home
+            123	foobar
+            456	club
+
+            /etc
+            789	book
+
+            /hello
+            999	moon
         """,
         [
-            Clause(Path("/usr/bin"), "garbage"),
-            Clause(Path("/usr/lib"), "library"),
-            Clause(Path("/usr/bin/X11"), "X10"),
-            Clause(Path("/usr/lib/X11"), "X12"),
-            Clause(Path("/usr/lib/games"), "play"),
-            Clause(Path("/usr/lib/gcc-lib"), "lib-gcc"),
-            Clause(Path("/usr/lib/locale"), "foreign"),
-            Clause(Path("/usr/lib/tls"), "osi"),
+            Clause(Path("/home/foo"), "foobar"),
+            Clause(Path("/home/bar"), "club"),
+            Clause(Path("/etc/lib"), "book"),
+            Clause(Path("/hello/world"), "moon"),
         ],
     ),
 ]
 
 
-@pytest.mark.parametrize("inode_paths, text, expected", edited_text_dataset)
-def test_parse_edited_text(inode_paths, text, expected):
+@pytest.mark.parametrize("title, inode_paths, text, expected", edited_text_dataset)
+def test_parse_edited_text(title, inode_paths, text, expected):
+    print(title)
     text = "\n".join(line.strip() for line in text.split("\n"))
     actual = parse_edited_text(text, inode_paths)
     assert actual == expected
 
 
-@pytest.mark.parametrize("inode_paths, text, expected", edited_text_dataset)
-def test_edit_paths(inode_paths, text, expected):
+@pytest.mark.parametrize("title, inode_paths, text, expected", edited_text_dataset)
+def test_edit_paths(title, inode_paths, text, expected):
+    print(title)
     actual = edit_paths(
         paths=inode_paths.values(),
         get_inode=lambda x: {path: inode for (inode, path) in inode_paths.items()}[x],
