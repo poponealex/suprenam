1ccc5b0a69ac65850356acf719e222cf733adff4
poponealex <74072635+poponealex@users.noreply.github.com>
2021-09-27 22:33:05 +0200

    Use `pathvalidate` to validate new filenames in `parse_edited_text`
    Add the `pathvalidate` dependency to check the new names from user input.
    Improve `parse_edited_text` with `pathvalidate.validate_filename`.
    Add missing types.
    Sort imports.

--- src/edition.py
+++ src/edition.py
@@ -1,8 +1,10 @@
-import re
+import subprocess
 from pathlib import Path
-from typing import List
+from pathvalidate import validate_filename
+from platform import platform
 from itertools import groupby
 from tempfile import NamedTemporaryFile
+from typing import List
 
 from src.default_editor import get_editor_command_name
 from src.user_types import Clause
@@ -34,21 +36,21 @@ def get_editable_text(inode_paths: dict) -> str:
     )
     # TODO for the name, use natural sort instead of lexicographic sort, and test thorougly
     groups = [
-        (parent, list(children)) # converting to list prevents len(groups) to consume the generator
+        (parent, list(children))  # converting to list prevents len(groups) to consume the generator
         for (parent, children) in groupby(paths_and_inodes, key=lambda item: item[0].parent)
     ]
-    if len(groups) > 1: 
+    if len(groups) > 1:
         for (parent, children) in groups:
-            result.append(f"\n{parent}\n") # empty line before each parent for maximal consistency
+            result.append(f"\n{parent}\n")  # empty line before each parent for maximal consistency
             for (path, inode) in children:
                 result.append(f"{inode}\t{path.name}\n")
-    else: # when all files to rename are siblings, it is useless to print their parent's path
+    else:  # when all files to rename are siblings, it is useless to print their parent's path
         for (path, inode) in groups[0][1]:
             result.append(f"{inode}\t{path.name}\n")
     return "".join(result)
 
 
-def get_editable_file_path(inode_paths):
+def get_editable_file_path(inode_paths: dict) -> Path:
     """
     Create a temporary file populated with the names of the files to rename.
 
@@ -69,7 +71,7 @@ def run_editor(editable_file_path: Path) -> str:
 
     Args:
         editable_file_path: Path to the temporary file populated with the editable text.
-    
+
     Returns:
         The text contained in editable_file_path when the user closes the editor's window.
     """
@@ -78,7 +80,7 @@ def run_editor(editable_file_path: Path) -> str:
     return editable_file_path.read_text()
 
 
-def parse_edited_text(text, inode_paths, find_all=re.compile(r"(?m)^(\d+)\t([^\t\n/]+)$").findall):
+def parse_edited_text(text: str, inode_paths, platform: str = platform().split("-")[0]) -> List[Clause]:
     """
     Parse the renamings' text.
 
@@ -91,9 +93,18 @@ def parse_edited_text(text, inode_paths, find_all=re.compile(r"(?m)^(\d+)\t([^\t
         A list of Clause (path, new_name).
     """
     result = []
-    for (inode, new_name) in find_all(text):
+    for line in text.split("\n"):
+        inode, *tail = line.split("\t", maxsplit=1)
+        if not inode.isdigit():
+            continue
         path = inode_paths.get(int(inode))
-        if path and Path(path).name != new_name:
+        if not path:
+            raise InodeError(f"This inode is invalid: '{inode}'.")
+        new_name = tail[0]
+        if "\t" in new_name:
+            raise TabError(f"Illegal \\t character in the new name: '{new_name}'.")
+        if Path(path).name != new_name:
+            validate_filename(new_name, platform=platform)
             result += [Clause(path, new_name)]
     return result
 
@@ -121,3 +132,11 @@ def edit_paths(
     text = edit(handler)
     clauses = parse_edited_text(text, inode_paths)
     return clauses
+
+
+class InodeError(ValueError):
+    ...
+
+
+class TabError(ValueError):
+    ...
