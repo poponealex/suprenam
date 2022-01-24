6a5a422f9d2fcfe0135bbd0f9e639c5cd3363e27
Alexandre <74072635+poponealex@users.noreply.github.com>
2022-01-24 11:39:39 +0100

    Tweak some details

--- build/platypus/create_platypus_app.sh
+++ build/platypus/create_platypus_app.sh
@@ -1,17 +1,23 @@
 #! /bin/sh
 
-REPO_ABSOLUTE_PATH=# FILL ME 
+REPO_ABSOLUTE_PATH=# FILL ME
+APP_DESTINATION=# FILL ME
 
 WRAPPER="$REPO_ABSOLUTE_PATH/build/platypus/wrapper.sh"
 SRC="$REPO_ABSOLUTE_PATH/src"
 BUNDLE="$REPO_ABSOLUTE_PATH/build/platypus/app_bundle"
 PYTHON_SITE_PACKAGES=$(pip3 show pathvalidate | grep "^Location" | cut -c 11-)
-
-APP_DESTINATION=# FILL ME
 APP_VERSION="1.0.0"
+PLATYPUS_SHARE="/usr/local/share/platypus"
 
 if [ ! -d "$REPO_ABSOLUTE_PATH" -o ! -d "$SRC" ]; then
     echo "FATAL ERROR: CHECK REPO'S PATH"
+    exit 2
+fi
+
+if [[ ! -d "$PLATYPUS_SHARE"  || ! -d "$PLATYPUS_SHARE/MainMenu.nib" || ! -f "$PLATYPUS_SHARE/ScriptExec" ]]; then
+    echo "FIX YOUR PLATYPUS INSTALL"
+    echo "Platypus share files ('MainMenu.nib' (dir) AND 'ScriptExec' (file)) have to be located at $PLATYPUS_SHARE"
     exit 2
 fi
 
@@ -38,7 +44,7 @@ for file in $(ls $SRC/*.py); do
     fi
 done
 
-cp -f "$SRC"/{suprenam.py,__init__.py} "$REPO/LICENSE" "$BUNDLE"
+cp -f "$SRC"/{suprenam.py,__init__.py} "$REPO_ABSOLUTE_PATH/LICENSE" "$BUNDLE"
 
 for file in $(ls $BUNDLE); do
     bundle_files="$bundle_files --bundled-file $BUNDLE/$file"
@@ -58,7 +64,7 @@ $bundle_files \
 --optimize-nib \
 --overwrite \
 "$WRAPPER" \
-"$APP_DESTINATION"
+"$APP_DESTINATION/Suprenam.app"
 
 rm -rf "$BUNDLE"
 exit
