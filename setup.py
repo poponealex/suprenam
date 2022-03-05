3cffc3671fda27f18a8f9b919eac8d2b2bf91a1d
Alexandre <74072635+poponealex@users.noreply.github.com>
2022-03-05 17:28:24 +0100

    Setup automatic deployment to PyPi

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="suprenam",
+    version=os.environ.get("RELEASE_VERSION")[1:],
+    author="Aristide Grange & Alexandre Perlmutter",
+    author_email="alexandre.perlmutter@gmail.com",
+    description="Easily rename files and folders via your favorite text editor.",
+    long_description=Path("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/poponealex/suprenam",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"src": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
