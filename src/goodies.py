79f473ac0b581723efd66fe9356165eb9ec2e76d
laowantong <laowantong@users.noreply.github.com>
2022-03-09 12:47:27 +0100

    improve code style and clarity

--- src/goodies.py
+++ src/goodies.py
@@ -2,34 +2,16 @@ import re
 from pathlib import Path
 from unicodedata import combining, normalize
 
-OUTLIERS = str.maketrans(
-    {
-        "ä": "ae",
-        "æ": "ae",
-        "ǽ": "ae",
-        "đ": "d",
-        "ð": "d",
-        "ƒ": "f",
-        "ħ": "h",
-        "ı": "i",
-        "ł": "l",
-        "ø": "o",
-        "ǿ": "o",
-        "ö": "oe",
-        "œ": "oe",
-        "ß": "ss",
-        "ŧ": "t",
-        "ü": "ue",
-    }
-)
+LATIN = "ä  æ  ǽ  đ ð ƒ ħ ı ł ø ǿ ö  œ  ß  ŧ ü "
+ASCII = "ae ae ae d d f h i l o o oe oe ss t ue"
 
 
-def remove_diacritics(s):
-    return "".join(c for c in normalize("NFD", s.lower().translate(OUTLIERS)) if not combining(c))
+def remove_diacritics(s: str, outliers=str.maketrans(dict(zip(LATIN.split(), ASCII.split())))):
+    return "".join(c for c in normalize("NFD", s.lower().translate(outliers)) if not combining(c))
 
 
-def string_to_pairs(s, texts_and_nats=re.compile(r"(\D*)(\d*)").findall):
-    return [(txt, int(nat or 0)) for (txt, nat) in texts_and_nats(s)][:-1]
+def string_to_pairs(s, pairs=re.compile(r"(\D*)(\d*)").findall):
+    return [(text, int(digits or 0)) for (text, digits) in pairs(s)[:-1]]
 
 
 def rm_tree(path: Path):  # https://stackoverflow.com/a/57892171/173003
